6204a7aa91db67ff975f2b2c1cbfb29344c16801
Bandeiraa <fcodavibandeira@edu.unifor.br>
2022-03-01 19:21:02 -0300

    Project Commit

---
+++ scripts/decoration/background.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+
+export(Array, int) var layer_speed #velocidade_camada
+export(bool) var can_move_layer #pode_mover_camada
+
+func _physics_process(delta: float) -> void: #1/60 = 0.0016667 -> float
+	if can_move_layer:
+		var index: int = 0
+		for parallax_layer in get_children():
+			parallax_layer.motion_offset.x -= layer_speed[index] * delta
+			index += 1
+			
